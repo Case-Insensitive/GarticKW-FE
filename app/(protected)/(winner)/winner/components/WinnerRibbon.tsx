31f9c483267eb916a6b2a03fed8168a69f5c8b60
Xrayya <nur.alkhiyar@gmail.com>
2024-12-25 17:57:11 +0700

    feat: winner ribbon

---
+++ app/(protected)/(winner)/winner/components/WinnerRibbon.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const WinnerRibbon = () => {
+  return (
+    <div className="flex justify-center">
+      <div className={`relative mt-[62px] w-[55%] h-[62px]`}>
+        <Image
+          alt="ribbon"
+          src={"/choose-category/Rectangle 50.png"}
+          className="absolute w-full h-full z-[-1]"
+          width={650}
+          height={65}
+        />
+        <h1 className="flex justify-center items-center w-full h-full font-black text-5xl text-white">
+          WINNER
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default WinnerRibbon;
---
+++ app/(protected)/(winner)/winner/page.tsx
@@ -0,0 +1,9 @@
+import WinnerRibbon from "./components/WinnerRibbon";
+
+const WinnerPage = () => {
+  return <>
+    <WinnerRibbon />
+  </>;
+};
+
+export default WinnerPage;
